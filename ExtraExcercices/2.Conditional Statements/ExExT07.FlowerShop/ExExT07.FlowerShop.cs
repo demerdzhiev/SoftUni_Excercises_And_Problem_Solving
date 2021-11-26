68e2d881bccd25f68e475d19b55afc6b1e0fa389
demerdzhiev <89989420+demerdzhiev@users.noreply.github.com>
2021-11-26 23:45:10 +0200

    Initial commit

---
+++ ExtraExcercices/2.Conditional Statements/ExExT07.FlowerShop/ExExT07.FlowerShop.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ExExT07.FlowerShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double m = double.Parse(Console.ReadLine());
+            double z = double.Parse(Console.ReadLine());
+            double r = double.Parse(Console.ReadLine());
+            double k = double.Parse(Console.ReadLine());
+            double giftPrice = double.Parse(Console.ReadLine());
+
+            double mPrice = 3.25;
+            double zPrice = 4;
+            double rPrice = 3.50;
+            double kPrice = 8;
+            double profit = m * mPrice + z * zPrice + r * rPrice + k * kPrice;
+            double profitAfterTaxes = profit * 0.95;
+
+            if (profitAfterTaxes >= giftPrice)
+            {
+                double moneyLeft = Math.Floor(profitAfterTaxes - giftPrice);
+                Console.WriteLine($"She is left with {moneyLeft} leva.");
+            }
+            else if (giftPrice > profitAfterTaxes)
+            {
+                double moneyNeeded = Math.Ceiling(giftPrice - profitAfterTaxes);
+                Console.WriteLine($"She will have to borrow {moneyNeeded} leva.");
+            }
+
+            
+        }
+    }
+}
---
+++ Lab/LectureExcercies/LabT07.ToyShop.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace LectureExcercies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double tripPrice = double.Parse(Console.ReadLine());
+            int puzzle = int.Parse(Console.ReadLine());
+            int dolls = int.Parse(Console.ReadLine());
+            int bears = int.Parse(Console.ReadLine());
+            int minions = int.Parse(Console.ReadLine());
+            int trucks = int.Parse(Console.ReadLine());
+
+            double puzzlePrice = 2.6;
+            double dollsPrice = 3;
+            double bearsPrice = 4.1;
+            double minionsPrice = 8.2;
+            double trucksPrice = 2;
+            double price = (puzzle * puzzlePrice) + (dolls * dollsPrice) + (bears * bearsPrice) + (minions * minionsPrice) + (trucks * trucksPrice);
+            double toys = puzzle + dolls + bears + minions + trucks;
+
+            if (toys >= 50)
+            {
+                price = price - (price * 0.25);
+            }
+
+            price = price - (price * 0.1);
+
+            if (price >= tripPrice)
+            {
+                double enoughMoney = price - tripPrice;
+                Console.WriteLine($"Yes! {(enoughMoney):f2} lv left.");
+            }
+            else
+            {
+                double moneyNeeded = tripPrice - price;
+                Console.WriteLine($"Not enough money! {(moneyNeeded):f2} lv needed.");
+
+            }
+
+        }
+    }
+}
